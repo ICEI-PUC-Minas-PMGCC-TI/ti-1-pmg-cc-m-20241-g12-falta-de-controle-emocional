445aa39c798c23a4ba7db34470e36f68ac4ab1f5
marialmeida1 <marianaalmeidafga@gmail.com>
2024-05-22 14:16:47 -0300

    feat: preparando para a criação do delete e edit de psicologos

--- codigo/assets/js/pages/cadastro_psicologos.js
+++ codigo/assets/js/pages/cadastro_psicologos.js
@@ -1,7 +1,10 @@
-const url = "http://localhost:3000/atendimentos";
-const select = document.getElementById("select_atd");
+// URLs
+const url_atd = "http://localhost:3000/atendimentos";
+const url_psi = "http://localhost:3000/psicologos";
 
-fetch(url)
+// Mostra tipos de atendimentos
+const select = document.getElementById("select_atd");
+fetch(url_atd)
    .then((resp) => resp.json())
    .then(function (data) {
       let atds = data;
@@ -23,3 +26,50 @@ function createList(element) {
 function append(parent, el) {
    return parent.appendChild(el);
 }
+
+// Cadastra psicólogos
+document.getElementById("form_psi").addEventListener("submit", async function (event) {
+   event.preventDefault();
+
+   const form_data = new FormData(this);
+
+   // Pegar qtd de psicologos cadastrados
+   try {
+      const newId = Math.floor(Math.random() * 100);
+
+      const data = {
+         id: newId,
+         cpf: form_data.get("cpf"),
+         cepp: form_data.get("cepp"),
+         endereco: form_data.get("endereco"),
+         formacao: form_data.get("formacao"),
+         atendimento: parseInt(form_data.get("atendimento")),
+      };
+
+      var request = new Request(url_psi, {
+         method: "POST",
+         body: JSON.stringify(data),
+         headers: {
+            "Content-Type": "application/json",
+         },
+      });
+
+      fetch(request).then(function () {
+         console.log(request);
+      });
+
+      console.log(data);
+   } catch (error) {
+      console.error("Erro ao cadastrar novo psicólogo:", error);
+   }
+});
+
+// Edit Psicologo
+document.getElementById("form_psi_edit").addEventListener("submit", function () {
+   console.log("clicou no edit");
+});
+
+// Delete Psicologo
+document.getElementById("form_psi_delete").addEventListener("submit", function () {
+   console.log("clicou no delete");
+});
